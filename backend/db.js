ca00ed7404273414d8e1f5f5b8055b47d0c66704
skndrali22 <skndrshaikh17c@gmail.com>
2023-10-22 02:02:47 +0500

    Mongo db connection and express endpoint

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb+srv://sikandar:sikandar@cluster0.y7err7s.mongodb.net/?retryWrites=true&w=majority"
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+module.exports = connectToMongo;
