9c30139054b653b4541e517a6def594375f764ef
skndrali22 <skndrshaikh17c@gmail.com>
2023-10-22 15:43:57 +0500

    uploaded data on mongodb

--- backend/models/user.js
+++ backend/models/user.js
@@ -16,8 +16,8 @@ const UserSchema = new Schema({
         required: true
     },
     date:{
-        name: Date,
-        default: Date.now
+        type: Date,
+        default: Date.now  
     },
 });
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,43 @@
+// const express = require('express');
+// const router = express.Router();
+// const User = require('../models/User');
+
+// // Create a User using POST "/api/auth/"  doesn't require authentication
+
+// router.get('/' , (req , res)=>{
+
+//     console.log(req.body);
+//     const user = User(req.body);
+//     user.save();
+//     res.send(req.body);
+// } )
+
+// module.exports = router
+
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/' , (req , res)=>{
-    obj = {
-        aa:'thisos',
-        n:34
+router.post('/', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Check if the email already exists in the database
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already exists' });
     }
-    res.json(obj);
-} )
 
-module.exports = router
+    // Create and save the user if the email is unique
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    // Handle other errors, e.g., database connection issues
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+module.exports = router;
